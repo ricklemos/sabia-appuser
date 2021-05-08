7b2d1ca57e1d1ee2bee328f13a208ba42c227083
gabrielmd96 <gmduarte96@gmail.com>
2021-05-08 19:34:08 -0300

    feat: firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFirestoreModule } from  '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,13 @@ export const environment = {
   apiUrl: '',
   baseUrl: '',
   version: require('../../package.json').version,
-  build: require('../../package.json').build
+  build: require('../../package.json').build,
+  firebaseConfig: {
+    apiKey: "AIzaSyDPBGUz1OUUDuZ9tEK28ALTE8697s79foM",
+    authDomain: "fpgr-tcc.firebaseapp.com",
+    projectId: "fpgr-tcc",
+    storageBucket: "fpgr-tcc.appspot.com",
+    messagingSenderId: "456482136613",
+    appId: "1:456482136613:web:1311813644a656750295cc"
+  }
 };

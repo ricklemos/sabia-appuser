7ec9554e33d4059c81819f7fe86fd201294dbdda
gabrielmd96 <gmduarte96@gmail.com>
2021-06-11 17:22:08 -0300

    feat: created authGuard and implemented in sessions/logged

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { UrlService } from '../services/url.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private angularFireAuth: AngularFireAuth,
+    private router: Router,
+    private urlService: UrlService
+  ) {
+  }
+
+  // canActivate(
+  //   route: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot
+  // ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
+
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> {
+    const user = await this.angularFireAuth.currentUser;
+    const isAuthenticated = user ? true : false;
+    if (!isAuthenticated) {
+      await this.router.navigate([this.urlService.getLoginUrl()], { queryParams: { returnUrl: state.url }});
+    }
+    return isAuthenticated;
+  }
+
+}

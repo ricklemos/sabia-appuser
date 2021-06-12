a175ad14a93706cacc53413d22e932db910b9ef2
Fernanda Parodi <nandaparodi@gmail.com>
2021-06-12 11:06:47 -0300

    feat: layout profile init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutSessionsComponent } from './sessions/layout/layout-sessions/layout-sessions.component';
 import { EditDataComponent } from './profile/components/edit-data/edit-data.component';
+import { ProfileViewComponent } from './profile/components/profile-view/profile-view.component';
 
 const routes: Routes = [
   {
@@ -14,7 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'profile',
-    component: EditDataComponent
+    component: ProfileViewComponent
   }
 ];
 
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditDataComponent } from './components/edit-data/edit-data.component';
 import { ModifyUserDataService } from './services/modify-user-data.service';
+import { ProfileViewComponent } from './components/profile-view/profile-view.component';
 
 
 
 @NgModule({
   declarations: [
     EditDataComponent,
+    ProfileViewComponent,
   ],
   imports: [
     CommonModule,

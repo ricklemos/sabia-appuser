57fffc928878e528d8c6ea5c208955d85f572093
Fernanda Parodi <nandaparodi@gmail.com>
2021-06-20 16:22:51 -0300

    feat: change password init

---
+++ src/app/profile/components/change-password/change-password.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ModifyUserDataService } from '../../services/modify-user-data.service';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UrlService } from '../../../services/url.service';
+import firebase from 'firebase';
+import { ChangePasswordPageComponent } from '../../containers/change-password-page/change-password-page.component';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.scss']
+})
+export class ChangePasswordComponent implements OnInit {
+  newPassword = new FormControl('');
+
+  constructor(
+    private modifyUserDataService: ModifyUserDataService,
+    private router: Router,
+    private urlService: UrlService,
+    private changePasswordPageComponent: ChangePasswordPageComponent
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  changePassword(): void {
+    const user = firebase.auth().currentUser;
+
+    user.updatePassword(this.newPassword.value).then(function () {
+      console.log(this.newPassword.value);
+    }).catch(function (error) {
+
+    });
+    this.router.navigate([this.urlService.getProfileUrl()]);
+  }
+}

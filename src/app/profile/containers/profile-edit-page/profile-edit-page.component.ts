aeef1ae7180d66301f385da572aa25edeb31ce66
Fernanda Parodi <nandaparodi@gmail.com>
2021-06-25 23:26:35 -0300

    feat: profile edit data layout done

--- src/app/profile/containers/profile-edit-page/profile-edit-page.component.ts
+++ src/app/profile/containers/profile-edit-page/profile-edit-page.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-profile-edit-page',
   templateUrl: './profile-edit-page.component.html',
   styleUrls: ['./profile-edit-page.component.scss']
 })
 export class ProfileEditPageComponent implements OnInit {
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }

4eb7f9edf76fe75a67e49bc41c5bf793ffdc68ee
gabrielmd96 <gmduarte96@gmail.com>
2021-07-22 16:49:55 -0300

    feat: finished questionary-done page

--- src/app/questionary/components/questionary-done/questionary-done.component.ts
+++ src/app/questionary/components/questionary-done/questionary-done.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UrlService } from '../../../services/url.service';
+import { QuestionaryService } from '../../services/questionary.service';
 
 @Component({
   selector: 'questionary-done',
@@ -11,13 +12,19 @@ export class QuestionaryDoneComponent implements OnInit {
 
   @Input() score: number;
   @Input() nQuestions;
+  grade: string;
+  questionaryName: string;
 
   constructor(
     private router: Router,
-    private urlService: UrlService
-  ) { }
+    private urlService: UrlService,
+    private questionaryService: QuestionaryService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.questionaryName = this.questionaryService.getQuestionary().questionaryName;
+    this.grade = Math.round((this.score / this.nQuestions) * 10).toFixed(1);
   }
 
   closeQuestionary(): void {

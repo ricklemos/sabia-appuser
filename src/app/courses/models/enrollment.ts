fdff43d570e9bffec8b4c7f638cb3336260ff8ce
Pri Veras <priveras01@gmail.com>
2021-08-17 19:23:31 -0300

    feat: courses enrollment models

---
+++ src/app/courses/models/enrollment.ts
@@ -0,0 +1,9 @@
+export interface Enrollment {
+  classroomId: string;
+  classroomName: string;
+  courseId: string;
+  courseName: string;
+  courseLink: string;
+  courseDescription?: string;
+  userId: string;
+}

0620ff2b4b02a71d0e84000cd6d1a746ea046a0c
Pri Veras <priveras01@gmail.com>
2021-08-17 19:33:29 -0300

    feat: courses module box init

---
+++ src/app/courses/components/courses-module-box/courses-module-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'courses-module-box',
+  templateUrl: './courses-module-box.component.html',
+  styleUrls: ['./courses-module-box.component.scss']
+})
+export class CoursesModuleBoxComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}

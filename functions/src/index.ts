dae631a24f7c92368671c2c81718d64c20dff1cd
gabrielmd96 <gmduarte96@gmail.com>
2021-08-27 17:24:00 -0300

    feat: createEnrollmentWhenCreateClassroom with ranking bug when there is a non assigned user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,201 +1,15 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { error } from 'firebase-functions/lib/logger';
+import { UserRanking } from './models';
 
 admin.initializeApp();
 
-export interface UserRanking {
-  userId: string;
-  userName: string;
-  userScore: number;
-}
+import * as tests from './tests';
 
-export interface QuestionnaireTemplate {
-  questionnaireId: string;
-  questionnaireName: string;
-  questions: string[];
-  score?: number;
-  tentatives?: number;
-}
+export const createMockupData = tests.createMockupData;
+export const createClassroom = tests.createClassroom;
 
-export interface QuestionTemplate {
-  questionId: string;
-  questionText: string;
-  alternatives: Alternative[];
-}
-
-export interface Alternative {
-  alternativeText: string;
-  isRight: boolean;
-}
-
-export const createMockupData = functions.https
-  .onRequest((req, res) => {
-    const users = [
-      {
-        userId: '01',
-        email: 'gmduarte96@gmail.com',
-        firstName: 'Gabriel',
-        lastName: 'Duarte'
-      },
-      {
-        userId: '02',
-        email: 'rick1@teste.com',
-        firstName: 'Rick',
-        lastName: 'Lemos'
-      },
-      {
-        userId: '03',
-        email: 'rick2@teste.com',
-        firstName: 'Rick',
-        lastName: 'Lemos'
-      },
-      {
-        userId: '04',
-        email: 'rick3@teste.com',
-        firstName: 'Rick',
-        lastName: 'Lemos'
-      },
-      {
-        userId: '05',
-        email: 'rick4@teste.com',
-        firstName: 'Rick',
-        lastName: 'Lemos'
-      }
-    ];
-    const questions = [
-      {
-        questionId: '01',
-        questionText: 'O joão roubou pão?',
-        alternatives: [
-          {
-            alternativeText: 'Sim',
-            isRight: true,
-          },
-          {
-            alternativeText: 'Não',
-            isRight: false,
-          }
-        ]
-      },
-      {
-        questionId: '02',
-        questionText: 'O rick roubou pão?',
-        alternatives: [
-          {
-            alternativeText: 'Sim',
-            isRight: true,
-          },
-          {
-            alternativeText: 'Não',
-            isRight: false,
-          }
-        ]
-      }
-    ];
-    const questionnaireTemplates: QuestionnaireTemplate[] = [
-      {
-        questionnaireId: '01',
-        questionnaireName: 'Teste do Pão',
-        questions: ['01', '02']
-      },
-      {
-        questionnaireId: '02',
-        questionnaireName: 'Teste do Pão 2',
-        questions: ['02', '01']
-      }
-    ];
-    const courseId = '02';
-    users.forEach(user => {
-      admin.firestore().doc(`/userData/${ user.userId }`)
-        .create(user)
-        .then(() => res.status(200).send('createUser Succesfully'))
-        .catch(err => res.status(500).send(err));
-    });
-    questionnaireTemplates.forEach((questionnaire: QuestionnaireTemplate) => {
-      admin.firestore().doc(`/questionnaireTemplate/${ questionnaire.questionnaireId }`)
-        .create(questionnaire)
-        .then(() => res.status(200).send('createQuestionnaire Succesfully'))
-        .catch(err => res.status(500).send(err));
-    });
-    questions.forEach((question: QuestionTemplate) => {
-      admin.firestore().doc(`/questionTemplate/${ question.questionId }`)
-        .create(question)
-        .then(() => res.status(200).send('createQuestionSuccesfully'))
-        .catch(err => res.status(500).send(err));
-    });
-    admin.firestore().doc(`/courseTemplate/${ courseId }`)
-      .create({
-        courseId: '02',
-        courseName: 'General Business Program',
-        modules: ['01', '02'],
-      })
-      .then(() => res.send('createClassroom Succesfully'))
-      .catch(err => console.log(err));
-    admin.firestore().doc(`/moduleTemplate/01`)
-      .create({
-        moduleId: '01',
-        moduleName: 'Renda Fixa',
-        lessons: [
-          {
-            lessonType: 'lesson',
-            complete: false,
-            lessonId: '01'
-          },
-          {
-            lessonType: 'lesson',
-            complete: false,
-            lessonId: '02'
-          },
-          {
-            lessonType: 'questionnaire',
-            complete: false,
-            questionnaireId: '01'
-          },
-          {
-            lessonType: 'questionnaire',
-            complete: false,
-            questionnaireId: '02'
-          }
-        ]
-      })
-      .then(() => res.send('createClassroom Succesfully'))
-      .catch(err => console.log(err));
-    admin.firestore().doc(`/moduleTemplate/02`)
-      .create({
-        moduleId: '02',
-        moduleName: 'Renda Variável',
-        lessons: [{
-          lessonType: 'lesson',
-          complete: false,
-          lessonId: '03'
-        },
-          {
-            lessonType: 'lesson',
-            complete: false,
-            lessonId: '04'
-          }]
-      })
-      .then(() => res.send('createClassroom Succesfully'))
-      .catch(err => console.log(err));
-  });
-
-export const createClassroom = functions.https
-  .onRequest((req, res) => {
-    const classroomId = '02';
-    admin.firestore().doc(`/classrooms/${ classroomId }`)
-      .create({
-        courseId: '02',
-        classroomId,
-        courseName: 'Renda Fixa',
-        classroomName: 'Turma 1',
-        institutionName: 'BTC',
-        modules: ['01', '02'],
-        students: ['gmduarte96@gmail.com', 'rick1@teste.com', 'rick2@teste.com', 'rick3@teste.com', 'rick4@teste.com'],
-      })
-      .then(() => res.send('createClassroom Succesfully'))
-      .catch(err => console.log(err));
-  });
 
 export const createSingleEnrollment = functions.firestore
   .document('classrooms/{classroomId}')
@@ -210,57 +24,72 @@ export const createEnrollmentWhenCreateClassroom = functions.firestore
   .document('classrooms/{classroomId}')
   .onCreate((snap) => {
     const data = snap.data();
+    // Verifica se os dados existem
     if (data) {
       const students = data.students;
       const classroomId = data.classroomId;
       const classroom = data;
       const ranking: UserRanking[] = [];
+      // Itera no array de students dos dados para criar as matrículas de cada aluno
       students.map((studentEmail: any, index: number) => {
         admin.firestore().collection('userData')
           .where('email', '==', studentEmail).get()
           .then((querySnapshot: any) => {
-            querySnapshot.forEach(async (user: any) => {
-              const userData = user.data();
-              const userId = user.id;
-              await admin.firestore().doc(`/enrollments/${ userId + '-' + classroomId }`)
-                .create({
-                  courseId: classroom.courseId,
-                  courseName: classroom.courseName,
-                  classroomName: classroom.classroomName,
-                  schoolName: classroom.institutionName,
-                  score: 0,
-                  classroomId,
-                  userId,
-                })
-                .then(() => {
-                  // Adiciona os dados desse usuário no vetor do ranking
-                  ranking.push({
+            // Verifica se o usuário existe no userData
+            if (!querySnapshot.empty) {
+              querySnapshot.forEach((user: any) => {
+                const userData = user.data();
+                const userId = user.id;
+                admin.firestore().doc(`/enrollments/${ userId + '-' + classroomId }`)
+                  .create({
+                    courseId: classroom.courseId,
+                    courseName: classroom.courseName,
+                    classroomName: classroom.classroomName,
+                    schoolName: classroom.institutionName,
+                    score: 0,
+                    classroomId,
                     userId,
-                    userName: userData.firstName + ' ' + userData.lastName,
-                    userScore: 0
-                  });
-                  // Se for o último aluno para cadastro, cria-se o ranking
-                  if (index === students.length - 1) {
-                    admin.firestore().doc(`/classRankings/${ classroomId }`)
-                      .create({
-                        courseId: classroom.courseId,
-                        courseName: classroom.courseName,
-                        classroomName: classroom.classroomName,
-                        schoolName: classroom.institutionName,
-                        ranking
-                      }).then(() => console.log('criou o ranking'));
-                  }
-                });
-            });
+                  })
+                  .then(() => {
+                    // Adiciona os dados desse usuário no vetor do ranking
+                    ranking.push({
+                      userId,
+                      userName: userData.firstName + ' ' + userData.lastName,
+                      userScore: 0
+                    });
+                    return ranking;
+                  })
+                  .then((userRanking) => {
+                    // Se for o último aluno para cadastro, cria-se o ranking
+                    if (index === students.length - 1) {
+                      console.log('ranking final:', userRanking);
+                      admin.firestore().doc(`/classRankings/${ classroomId }`)
+                        .create({
+                          courseId: classroom.courseId,
+                          courseName: classroom.courseName,
+                          classroomName: classroom.classroomName,
+                          schoolName: classroom.institutionName,
+                          userRanking
+                        }).then(() => console.log('criou o ranking'));
+                    }
+                  })
+                  .catch((err) => console.log('erro:', err));
+              });
+            } else {
+              // Aluno não é um usuário (adiciona em users sem conta)
+              admin.firestore().collection('users').add({ email: studentEmail })
+                .then(() => console.log('adicionou um novo usuário'))
+                .catch((err) => console.log(err));
+            }
           })
           .catch((err: any) => console.log(err));
-        return ranking;
       });
     } else {
       functions.logger
         .info('Dados da classe não existem', { structuredData: true });
     }
-  });
+  })
+;
 
 export const createmoduleProgressWhenCreateEnrollment = functions.firestore
   .document('enrollments/{enrollmentId}')
@@ -316,29 +145,7 @@ export const createQuestionnaireAnswerWhenCreateModuleProgress = functions.fires
             .then(doc => {
               const questionnaireTemplate = doc.data();
               if (questionnaireTemplate) {
-                // const questions: any = [];
-                // questionnaireTemplate.questions.forEach(async (question: any, index: number) => {
-                //   await admin.firestore().doc(`questionTemplate/${ question.questionId }`).get()
-                //     .then(questionDoc => {
-                //       if (questionDoc) {
-                //         const questionData = questionDoc.data();
-                //         questions.push(questionData);
-                //       }
-                //       return index, questions;
-                //     })
-                //     .then((docSnap) => {
-                //       console.log(docSnap.index, docSnap.questions);
-                //       if (docSnap.index === questionnaireTemplate.questions.length - 1){
-                //         admin.firestore().doc(`questionnaireAnswers/${ moduleProgress.userId + '-' + questionnaireId }`)
-                //           .create({
-                //             userId: moduleProgress.userId,
-                //             questionnaireId,
-                //             questionnaireName: questionnaireTemplate.questionnaireName,
-                //             questions
-                //           });
-                //       }
-                //     });
-                // });
+                // O "in" limita a query a 10 objetos. Assim, um questionário não pode ter mais de 10 questões.
                 admin.firestore().collection('questionTemplate')
                   .where('questionId', 'in', questionnaireTemplate.questions)
                   .get()
@@ -353,11 +160,11 @@ export const createQuestionnaireAnswerWhenCreateModuleProgress = functions.fires
                         questions
                       });
                   })
-                  .catch(err => console.log(err));
+                  .catch(err => console.log('erro:', err));
               } else {
                 throw error('questionnaire does not exist');
               }
-            }).then((done) => console.log(done)).catch((err) => console.log(err));
+            }).catch((err) => console.log('erro:', err));
         }
       });
     } else {

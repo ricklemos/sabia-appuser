755208a2eb461bc3eb7b758c3398448bd14d22fb
Pri Veras <priveras01@gmail.com>
2021-08-30 20:26:08 -0300

    fix : button "mark as done"

--- src/app/module/containers/module-content-page/module-content-page.component.ts
+++ src/app/module/containers/module-content-page/module-content-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { tap } from 'rxjs/operators';
 import { noop } from 'rxjs';
 import { ModuleContentService } from '../../services/module-content.service';
-import { Lesson, ModuleLesson, ModuleProgress } from '../../models/module';
+import { Lesson, ModuleProgress } from '../../models/module';
 import { ModuleService } from '../../services/module.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -15,7 +15,6 @@ export class ModuleContentPageComponent implements OnInit, OnDestroy {
 
   lesson: Lesson;
   moduleProgress: ModuleProgress;
-  moduleLesson: ModuleLesson;
   lessonDone: boolean;
 
   subscriptions = [];
@@ -31,15 +30,12 @@ export class ModuleContentPageComponent implements OnInit, OnDestroy {
     const fetchModule = this.moduleContentService.fetchLesson(this.route.snapshot.paramMap.get('lessonId')).pipe(
       tap(data => {
         this.lesson = data;
+        this.moduleProgress = this.moduleService.getModule();
+        const fetchLesson = this.moduleProgress.lessons.filter(lesson => lesson.lessonId === this.lesson.lessonId);
+        this.lessonDone = fetchLesson[0].complete;
       })
     ).subscribe(noop);
     this.subscriptions.push(fetchModule);
-
-    this.moduleProgress = this.moduleService.getModule();
-    console.log(this.moduleProgress);
-    const fetchLesson = this.moduleProgress.lessons.filter(lesson => lesson.lessonId === this.lesson.lessonId);
-    this.lessonDone = fetchLesson[0].complete;
-    // TODO: resolver bug
   }
 
   ngOnDestroy(): void {
@@ -48,7 +44,7 @@ export class ModuleContentPageComponent implements OnInit, OnDestroy {
     this.moduleService.updateModule();
   }
 
-  concluirLicao(): void {
+  markAsDone(): void {
     this.lessonDone = !this.lessonDone;
     const filteredLesson = this.moduleProgress.lessons.filter(lesson => lesson.lessonId === this.lesson.lessonId);
     filteredLesson[0].complete = this.lessonDone;

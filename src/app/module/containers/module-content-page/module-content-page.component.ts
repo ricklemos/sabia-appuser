7584a9f12579352f783d8634ba79a8d7d71fbf3c
Pri Veras <priveras01@gmail.com>
2021-08-30 18:15:53 -0300

    feat : button lesson complete

--- src/app/module/containers/module-content-page/module-content-page.component.ts
+++ src/app/module/containers/module-content-page/module-content-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { tap } from 'rxjs/operators';
 import { noop } from 'rxjs';
 import { ModuleContentService } from '../../services/module-content.service';
-import { Lesson, ModuleProgress } from '../../models/module';
+import { Lesson, ModuleLesson, ModuleProgress } from '../../models/module';
 import { ModuleService } from '../../services/module.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -15,6 +15,8 @@ export class ModuleContentPageComponent implements OnInit, OnDestroy {
 
   lesson: Lesson;
   moduleProgress: ModuleProgress;
+  moduleLesson: ModuleLesson;
+  lessonDone: boolean;
 
   subscriptions = [];
 
@@ -34,10 +36,22 @@ export class ModuleContentPageComponent implements OnInit, OnDestroy {
     this.subscriptions.push(fetchModule);
 
     this.moduleProgress = this.moduleService.getModule();
+    console.log(this.moduleProgress);
+    const fetchLesson = this.moduleProgress.lessons.filter(lesson => lesson.lessonId === this.lesson.lessonId);
+    this.lessonDone = fetchLesson[0].complete;
+    // TODO: resolver bug
   }
 
   ngOnDestroy(): void {
     this.subscriptions.map(u => u.unsubscribe);
+    this.moduleService.setModule(this.moduleProgress);
+    this.moduleService.updateModule();
+  }
+
+  concluirLicao(): void {
+    this.lessonDone = !this.lessonDone;
+    const filteredLesson = this.moduleProgress.lessons.filter(lesson => lesson.lessonId === this.lesson.lessonId);
+    filteredLesson[0].complete = this.lessonDone;
   }
 
 }

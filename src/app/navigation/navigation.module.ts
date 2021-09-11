11c31987dc044d181049617aaa93f1f1e941e65d
Ricardo Lemos <ricardo@collact.com.br>
2021-09-11 10:51:50 -0300

    feat: navbar-header

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -4,11 +4,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CollactComponentsModule } from 'collact-components';
 import { CollactDesignSystemModule } from 'collact-design-system';
 import { FlexModule } from '@angular/flex-layout';
+import { NavbarHeaderComponent } from './navbar-header/navbar-header.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
+    NavbarHeaderComponent,
   ],
   exports: [
     NavbarComponent

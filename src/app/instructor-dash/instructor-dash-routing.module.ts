be6a8391a002de37b527e13b8347555ba719165e
gabrielmd96 <gmduarte96@gmail.com>
2021-09-12 14:13:12 -0300

    fix: auth guard for instructor-dash pages

--- src/app/instructor-dash/instructor-dash-routing.module.ts
+++ src/app/instructor-dash/instructor-dash-routing.module.ts
@@ -10,7 +10,7 @@ export const InstructorDashRoutes: Routes = [
     children: [
       {
         path: 'classrooms',
-        // canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
         children: [
           {
             path: 'create',

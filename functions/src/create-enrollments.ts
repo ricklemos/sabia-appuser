3c79fadcfd06f95cb89b68a0ec63928c443cec6b
gabrielmd96 <gmduarte96@gmail.com>
2021-09-22 11:14:14 -0300

    fix: createQuestionnaireAnswers

--- functions/src/create-enrollments.ts
+++ functions/src/create-enrollments.ts
@@ -60,6 +60,18 @@ async function createEnrollmentFromClassroom(classroom: any, userId: string): Pr
     });
 }
 
+async function createQuestionnaire(userId: string, questionnaireTemplate: any, classroomId: string, moduleId: string): Promise<WriteResult>{
+  return admin.firestore().doc(`questionnaireAnswers/${ userId + '-' + questionnaireTemplate.questionnaireId }`)
+    .create({
+      userId,
+      questionnaireId: questionnaireTemplate.questionnaireId,
+      questionnaireName: questionnaireTemplate.questionnaireName,
+      questions: questionnaireTemplate.questions,
+      classroomId,
+      moduleId
+    });
+}
+
 export const createEnrollmentWhenCreateUserData = functions.firestore
   .document('userData/{userId}')
   .onCreate(async (snap) => {
@@ -82,89 +94,6 @@ export const createEnrollmentWhenCreateUserData = functions.firestore
       return Promise.all(promises);
     } else {
       return null;
-    }
-  });
-
-// ToDo: Refactor
-export const createEnrollmentWhenCreateOrUpdateClassroombackup = functions.firestore
-  .document('classrooms/{classroomId}')
-  .onWrite((change) => {
-    const data = change.after.data();
-    // Verifica se os dados existem
-    if (data) {
-      const students = data.students;
-      const classroomId = data.classroomId;
-      const classroom = data;
-      // Cria o Ranking da turma com array vazio
-      admin.firestore().doc(`/classRankings/${ classroomId }`)
-        .create({
-          classroomId,
-          courseId: classroom.courseId,
-          courseName: classroom.courseName,
-          classroomName: classroom.classroomName,
-          institutionName: classroom.institutionName,
-        }).then(() => console.log(`criou o ranking da turma ${ classroomId }`));
-      // Itera no array de students dos dados para criar as matrículas de cada aluno
-      students.map((studentEmail: any) => {
-        admin.firestore().collection('userData')
-          .where('email', '==', studentEmail).get()
-          .then((querySnapshot: any) => {
-            // Verifica se o usuário existe no userData
-            if (!querySnapshot.empty) {
-              querySnapshot.forEach((user: any) => {
-                const userData = user.data();
-                const userId = user.id;
-                // Cria o enrollment do aluno
-                admin.firestore().doc(`/enrollments/${ userId + '-' + classroomId }`)
-                  .create({
-                    courseId: classroom.courseId,
-                    courseName: classroom.courseName,
-                    classroomName: classroom.classroomName,
-                    institutionName: classroom.institutionName,
-                    courseDescription: classroom.courseDescription,
-                    courseLink: classroom.courseLink,
-                    score: 0,
-                    classroomId,
-                    userId,
-                  })
-                  .then(() => {
-                    // Atualiza o ranking com o aluno novo
-                    admin.firestore().doc(`/classRankings/${ classroomId }`)
-                      .update({
-                        ranking: admin.firestore.FieldValue.arrayUnion({
-                          userId,
-                          userName: userData.firstName + ' ' + userData.lastName,
-                          userScore: 0
-                        })
-                      });
-                  })
-                  .catch((err) => console.log('erro:', err));
-              });
-            } else {
-              // Aluno não é um usuário (adiciona em users sem conta com pre-enrollment: classroomId)
-              admin.firestore().collection('users').doc(studentEmail).set(
-                {
-                  email: studentEmail,
-                  preEnrollments: admin.firestore.FieldValue.arrayUnion({
-                    courseId: classroom.courseId,
-                    courseName: classroom.courseName,
-                    classroomName: classroom.classroomName,
-                    institutionName: classroom.institutionName,
-                    courseDescription: classroom.courseDescription,
-                    courseLink: classroom.courseLink,
-                    classroomId
-                  })
-                }
-                , { merge: true })
-                .then(() => console.log('adicionou um novo usuário'))
-                .catch((err) => console.log(err));
-            }
-          })
-          .catch((err: any) => console.log(err));
-      });
-    } else {
-      functions.logger
-        .info('Dados da classe não existem', { structuredData: true });
     }
   });
 
@@ -269,135 +198,37 @@ export const createmoduleProgressWhenCreateEnrollment = functions.firestore
     }
   });
 
-export const createmoduleProgressWhenCreateEnrollmentBackup = functions.firestore
-  .document('enrollments/{enrollmentId}')
-  .onCreate((snap) => {
-    const enrollment = snap.data();
-    if (enrollment) {
-      const userId = enrollment.userId;
-      admin.firestore().doc(`courseTemplate/${ enrollment.courseId }`).get()
-        .then((query) => {
-          const course = query.data();
-          if (course) {
-            course.modules.forEach((moduleId: string) => {
-              admin.firestore().doc(`moduleTemplate/${ moduleId }`).get()
-                .then((module) => {
-                  const moduleData = module.data();
-                  if (moduleData) {
-                    admin.firestore().doc(`moduleProgress/${ userId + '-' + moduleData.moduleId }`)
-                      .create({
-                        userId,
-                        moduleId,
-                        moduleName: moduleData.moduleName,
-                        institutionName: enrollment.institutionName,
-                        lessons: moduleData.lessons,
-                        score: 0,
-                        classroomId: enrollment.classroomId,
-                        moduleProgressPercentage: 0,
-                        courseId: enrollment.courseId,
-                      });
-                  } else {
-                    throw Error('No Module Data');
-                  }
-                })
-                .catch(err => console.log(err));
-            });
-          } else {
-            throw Error('No Course Data');
-          }
-        })
-        .catch(err => console.log(err));
-    } else {
-      functions.logger
-        .info('Dados da matrícula não existem', { structuredData: true });
-    }
-  });
-
-// ToDo: Refactor
-export const createQuestionnaireAnswerWhenCreateModuleProgressBackup = functions.firestore
-  .document('moduleProgress/{moduleProgressId}')
-  .onCreate((snap) => {
-    const moduleProgress = snap.data();
-    if (moduleProgress) {
-      const lessons = moduleProgress.lessons;
-      lessons.forEach((lesson: any) => {
-        if (lesson.lessonType === 'QUESTIONNAIRE') {
-          const questionnaireId = lesson.questionnaireId;
-          admin.firestore().doc(`questionnaireTemplate/${ questionnaireId }`).get()
-            .then(doc => {
-              const questionnaireTemplate = doc.data();
-              if (questionnaireTemplate) {
-                // O "in" limita a query a 10 objetos. Assim, um questionário não pode ter mais de 10 questões.
-                admin.firestore().collection('questionTemplate')
-                  .where('questionId', 'in', questionnaireTemplate.questions)
-                  .get()
-                  .then((questionsData) => {
-                    const questions: any[] = [];
-                    questionsData.docs.forEach(questionDoc => questions.push(questionDoc.data()));
-                    admin.firestore().doc(`questionnaireAnswers/${ moduleProgress.userId + '-' + questionnaireId }`)
-                      .create({
-                        userId: moduleProgress.userId,
-                        questionnaireId,
-                        questionnaireName: questionnaireTemplate.questionnaireName,
-                        questions,
-                        classroomId: moduleProgress.classroomId,
-                        moduleId: moduleProgress.moduleId
-                      });
-                  })
-                  .catch(err => console.log('erro:', err));
-              } else {
-                functions.logger
-                  .info('Questionnaire não existe', { structuredData: true });
-              }
-            }).catch((err) => console.log('erro:', err));
-        }
-      });
-    } else {
-      functions.logger
-        .info('Dados do moduleProgress não existem', { structuredData: true });
-    }
-  });
-
 export const createQuestionnaireAnswerWhenCreateModuleProgress = functions.firestore
   .document('moduleProgress/{moduleProgressId}')
-  .onCreate((snap) => {
+  .onCreate(async (snap) => {
+    const promises = [];
     const moduleProgress = snap.data();
     if (moduleProgress) {
       const lessons = moduleProgress.lessons;
-      lessons.forEach((lesson: any) => {
-        if (lesson.lessonType === 'QUESTIONNAIRE') {
-          const questionnaireId = lesson.questionnaireId;
-          admin.firestore().doc(`questionnaireTemplate/${ questionnaireId }`).get()
-            .then(doc => {
-              const questionnaireTemplate = doc.data();
-              if (questionnaireTemplate) {
-                // O "in" limita a query a 10 objetos. Assim, um questionário não pode ter mais de 10 questões.
-                admin.firestore().collection('questionTemplate')
-                  .where('questionId', 'in', questionnaireTemplate.questions)
-                  .get()
-                  .then((questionsData) => {
-                    const questions: any[] = [];
-                    questionsData.docs.forEach(questionDoc => questions.push(questionDoc.data()));
-                    admin.firestore().doc(`questionnaireAnswers/${ moduleProgress.userId + '-' + questionnaireId }`)
-                      .create({
-                        userId: moduleProgress.userId,
-                        questionnaireId,
-                        questionnaireName: questionnaireTemplate.questionnaireName,
-                        questions,
-                        classroomId: moduleProgress.classroomId,
-                        moduleId: moduleProgress.moduleId
-                      });
-                  })
-                  .catch(err => console.log('erro:', err));
-              } else {
-                functions.logger
-                  .info('Questionnaire não existe', { structuredData: true });
-              }
-            }).catch((err) => console.log('erro:', err));
+      const questionnaires = lessons.filter((lesson: any) => lesson.lessonType === 'QUESTIONNAIRE');
+      for (const questionnaire of questionnaires) {
+        const questionnaireId = questionnaire.questionnaireId;
+        const questionnaireTemplateQuery = await admin.firestore().doc(`questionnaireTemplate/${ questionnaireId }`).get();
+        const questionnaireTemplate = questionnaireTemplateQuery?.data() ?? null;
+        if (questionnaireTemplate) {
+          // O "in" limita a query a 10 objetos. Assim, um questionário não pode ter mais de 10 questões.
+          const questionsData = await admin.firestore().collection('questionTemplate')
+            .where('questionId', 'in', questionnaireTemplate.questions).get();
+          const questions: any[] = [];
+          questionsData.docs.forEach(questionDoc => questions.push(questionDoc.data()));
+          questionnaireTemplate.questions = questions;
+          const p = createQuestionnaire(moduleProgress.userId, questionnaireTemplate, moduleProgress.classroomId, moduleProgress.moduleId);
+          promises.push(p);
+        } else {
+          functions.logger
+            .info('Questionnaire não existe', { structuredData: true });
+          return null;
         }
-      });
+      }
+      return Promise.all(promises);
     } else {
       functions.logger
         .info('Dados do moduleProgress não existem', { structuredData: true });
+      return null;
     }
   });

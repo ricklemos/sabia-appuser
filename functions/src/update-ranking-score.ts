dc57960a137c6ee6304b31d0f46d4859fe55e721
gabrielmd96 <gmduarte96@gmail.com>
2021-09-22 14:57:57 -0300

    refactor: updateRanking and questionnaireUpdated (score) cloud functions

--- functions/src/update-ranking-score.ts
+++ functions/src/update-ranking-score.ts
@@ -1,7 +1,6 @@
 // Atualiza o Score do enrollment e do moduleProgress quando termina de responder o questionário pela primeira vez
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-// ToDo: Refatorar com return
 export const questionnaireUpdated = functions.firestore.document('/questionnaireAnswers/{questionnaireId}')
   .onUpdate((snap) => {
     const data = snap.after.data();
@@ -12,45 +11,46 @@ export const questionnaireUpdated = functions.firestore.document('/questionnaire
       const score = data.score;
       if (data.tentatives === 1) {
         // Incrementa o valor do score tanto em enrollments quanto no moduleProgress
-        admin.firestore().doc(`/enrollments/${ userId + '-' + classroomId }`)
+        const p1 = admin.firestore().doc(`/enrollments/${ userId + '-' + classroomId }`)
           .update({ score: admin.firestore.FieldValue.increment(score) })
           .catch((err) => console.log(err));
-        admin.firestore().doc(`/moduleProgress/${ userId + '-' + moduleId }`)
+        const p2 = admin.firestore().doc(`/moduleProgress/${ userId + '-' + moduleId }`)
           .update({ score: admin.firestore.FieldValue.increment(score) })
           .catch((err) => console.log(err));
+        return Promise.all([p1, p2]);
       } else {
-        console.log('não foi a primeira tentativa');
+        console.log('Não foi a primeira tentativa');
+        return null;
       }
     } else {
       console.log('data does not exist');
+      return null;
     }
   });
 
 // Atualiza o Score do Ranking quando atualiza o enrollment
 export const updateRanking = functions.firestore.document('/enrollments/{enrollmentId}')
-  .onUpdate((snap) => {
+  .onUpdate(async (snap) => {
     const data = snap.after.data();
     if (data) {
       const userId = data.userId;
       const score = data.score;
       const classroomId = data.classroomId;
-      admin.firestore().doc(`/classRankings/${ classroomId }`).get()
-        .then((doc) => {
-          const classRankingData = doc.data();
-          if (classRankingData) {
-            classRankingData.ranking.map((user: any) => {
-              if (user.userId === userId) {
-                user.userScore = score;
-              }
-            });
-            return admin.firestore().doc(`/classRankings/${ classroomId }`).update({ ranking: classRankingData.ranking });
-          } else {
-            console.log('data does not exist');
-            return null;
+      const classRankingDoc = await admin.firestore().doc(`/classRankings/${ classroomId }`).get();
+      const classRankingData = classRankingDoc.data();
+      if (classRankingData) {
+        classRankingData.ranking.map((user: any) => {
+          if (user.userId === userId) {
+            user.userScore = score;
           }
-        })
-        .catch(err => console.log(err));
+        });
+        return admin.firestore().doc(`/classRankings/${ classroomId }`).update({ ranking: classRankingData.ranking });
+      } else {
+        console.log('data does not exist');
+        return null;
+      }
     } else {
       console.log('data does not exist');
+      return null;
     }
   });

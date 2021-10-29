8358b3d63659e1f6d66c8e7350c61f78197f4e93
Pri Veras <priveras01@gmail.com>
2021-10-29 19:33:26 -0300

    feat : investment wallet pages created

---
+++ src/app/investment-wallet/containers/investment-wallet-products-page/investment-wallet-products-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'investment-wallet-products-page',
+  templateUrl: './investment-wallet-products-page.component.html',
+  styleUrls: ['./investment-wallet-products-page.component.scss']
+})
+export class InvestmentWalletProductsPageComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/investment-wallet/containers/investment-wallet-trade-page/investment-wallet-trade-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'investment-wallet-trade-page',
+  templateUrl: './investment-wallet-trade-page.component.html',
+  styleUrls: ['./investment-wallet-trade-page.component.scss']
+})
+export class InvestmentWalletTradePageComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
